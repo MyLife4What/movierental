a15c6199b3b48f1aaf9dbd28f97bab294b095261
MyLife4What <raweerot.p@ku.th>
2021-11-07 21:12:26 +0700

    Add a Movie Catalog in movie.py

--- movie.py
+++ movie.py
@@ -1,12 +1,13 @@
 """Title and price code calculator of movie."""
 from typing import List
+import csv
 
 
 class Movie:
     """A movie available for rent."""
 
     def __init__(self, title: str, year: int, genre: List[str]):
-        # Initialize a new movie.
+        """Initialize a new movie."""
         self._title = title
         self._year = year
         self._genre = genre
@@ -26,3 +27,23 @@ class Movie:
     def __str__(self):
         """Return title."""
         return self._title
+
+
+class MovieCatalog:
+    """A list of the movie that customer can rent."""
+
+    def __init__(self):
+        """Initialize a new movie."""
+        self.movie_list = {}
+        with open('movies.csv') as file:
+            reader = csv.DictReader(file)
+        for i in reader:
+            self.movie_list[i["title"]] = {
+                "#id": i["#id"],
+                "year": i["year"],
+                "genre": [j for j in i["genres"].split("|")]
+            }
+
+    def get_movie(self, title):
+        movie = self.movie_list[title]
+        return Movie(title, movie["year"], movie["genre"])
